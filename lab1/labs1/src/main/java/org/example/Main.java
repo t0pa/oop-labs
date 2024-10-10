98d0b2032630cb3c22b045ac76201cf54ea82ad8
Tarik <topictarik82@gmail.com>
2024-10-10 21:58:42 +0200

    firstlab

--- lab1/labs1/src/main/java/org/example/Main.java
+++ lab1/labs1/src/main/java/org/example/Main.java
@@ -1,17 +1,76 @@
 package org.example;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+
+
+        printName();
+        // printMessage();
+        // calculateSecondsInYear();
+        // sumTwoNumbers();
+        // checkIfPositive();
+        // compareTwoNumbers();
+    }
+
+    public static void printName() {
+        System.out.println("Your Name");
+    }
+
+    public static void printMessage() {
+        System.out.println("This is a custom message.");
+    }
+
+    public static void calculateSecondsInYear() {
+        int daysInYear = 365;
+        int secondsInDay = 24 * 60 * 60; // hours * minutes * seconds
+        int secondsInYear = daysInYear * secondsInDay;
+
+        System.out.println("There are " + secondsInYear + " seconds in a year.");
+    }
+
+    public static void sumTwoNumbers() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("The sum of the two numbers is: " + sum);
+    }
+
+    public static void checkIfPositive() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
+    }
+
+    public static void compareTwoNumbers() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        if (num1 > num2) {
+            System.out.println(num1 + " is greater than " + num2);
+        } else if (num2 > num1) {
+            System.out.println(num2 + " is greater than " + num1);
+        } else {
+            System.out.println("Both numbers are equal.");
         }
     }
 }
